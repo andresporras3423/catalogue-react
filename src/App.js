52fb4ad02d6e347fc81edb861d9375153c9acba0
oscarporras02 <oscarrussi@outlook.com>
2021-03-18 20:40:23 -0500

    get all required data from api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import { useState, useEffect } from 'react';
+import { getAllPokemonData } from './apiData';
 import './App.css';
 
 function App() {
-  const [value, setValue] = useState('');
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0', {
-          method: 'GET',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-        });
-        const data = JSON.stringify(await response.json());
-        setValue(data);
-        console.log(`data from the api: ${data}`);
-        return data;
-      } catch {
-        return -1;
-      }
-    })();
-  });
+  const [values, setValues] = useState([]);
+
+  useEffect(async () => {
+    setValues(await getAllPokemonData());
+  }, []);
   return (
     <div className="App">
       <p>
-        {value}
+        {values.map(v => JSON.stringify(v)).join(',')}
       </p>
     </div>
   );
