c8e6c179cb78db8276dd38cbfec1d1e9ceda83e9
oscarporras02 <oscarrussi@outlook.com>
2021-03-23 19:29:32 -0500

    using useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
 import '../App.css';
+import { useEffect } from 'react';
+import useState from 'react-usestateref';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { addPokemon, addType } from '../actions/index';
 import PokemonList from './PokemonsList';
 import PokemonFilter from './PokemonFilter';
 import PokemonDetails from './PokemonDetails';
 
 function App(props) {
-  const { switchFilterPage } = props;
+  const { switchFilterPage, handleAddPokemon, handleAddType } = props;
+  const [initialState, setInitialState, refInitialState] = useState([]);
+  useEffect(async () => {
+    if (initialState.length === 0) {
+      const getPokemonData = async () => {
+        const response = await fetch('https://hidden-plateau-07048.herokuapp.com/pokemon/get', {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await response.json();
+        setInitialState(Object.values(data));
+      };
+      await getPokemonData();
+      console.log(refInitialState.current);
+      refInitialState.current.forEach((nPokemon, index) => {
+        handleAddPokemon({
+          id: index,
+          name: nPokemon.name,
+          abilities: nPokemon.abilities,
+          types: nPokemon.types,
+          image: nPokemon.image,
+        });
+        nPokemon.types.forEach(nType => handleAddType(nType));
+      });
+    }
+  }, [initialState]);
 
   const renderComponent = () => {
     if (switchFilterPage) {
@@ -35,12 +66,25 @@ const mapStateToProps = state => ({
   switchFilterPage: state.filter.switchFilterPage,
 });
 
+const mapDispatchToProps = dispatch => ({
+  handleAddPokemon: pokemon => {
+    dispatch(addPokemon(pokemon));
+  },
+  handleAddType: type => {
+    dispatch(addType(type));
+  },
+});
+
 App.propTypes = {
   switchFilterPage: PropTypes.bool,
+  handleAddPokemon: PropTypes.func,
+  handleAddType: PropTypes.func,
 };
 
 App.defaultProps = {
   switchFilterPage: true,
+  handleAddPokemon: null,
+  handleAddType: null,
 };
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
